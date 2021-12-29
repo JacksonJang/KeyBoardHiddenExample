e67c78aa87d872d8e72fb27c38899de8817c1dec
장효원 <hyo961015@naver.com>
2021-12-29 18:27:29 +0900

    Add UITabGestureRecognizer of ScrollView

--- KeyBoardHiddenExample/Views/ScrollViewController.swift
+++ KeyBoardHiddenExample/Views/ScrollViewController.swift
@@ -8,8 +8,23 @@
 import UIKit
 
 class ScrollViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createEndEditingForScrollView()
+    }
+    
+    func createEndEditingForScrollView() {
+        let singleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(eventEndEdit))
+        singleTapGestureRecognizer.numberOfTapsRequired = 1
+        singleTapGestureRecognizer.isEnabled = true
+        singleTapGestureRecognizer.cancelsTouchesInView = false
+        scrollView.addGestureRecognizer(singleTapGestureRecognizer)
+    }
+    
+    @objc func eventEndEdit() {
+        self.view.endEditing(true)
     }
 }
